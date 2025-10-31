a4155b22c5571f705923f94dd37c6897a9b29722
Irvan Tijani <irvan.tijani@gmail.com>
2025-11-01 04:50:45 +0700

    fix:raport-pdf

--- resources/views/raport/pdf.blade.php
+++ resources/views/raport/pdf.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>{{ $filename }}</title>
+    <title>Raport {{ $report->student->name }}</title>
     <link rel="stylesheet" href="{{ asset('css/report/report-style.css') }}">
 </head>
 
